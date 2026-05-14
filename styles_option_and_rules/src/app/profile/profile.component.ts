80e8e066379d847a8251d66cb16e6716b0fa139b
parshu <parshuvmahindrakar@gmail.com>
2026-05-14 19:20:31 +0530

    Styles option and rules (css)

---
+++ styles_option_and_rules/src/app/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.component.html',
+  // styleUrl: './profile.component.css'
+  // styles: ['h1{background-color: lightblue; color: black;}'],
+  styleUrls: ['./profile.component.css', './profile.btn.css'],
+})
+export class ProfileComponent {}
