6bce1e2a6cb8b0ec6ca718d6130cc8076e534c7c
parshu <parshuvmahindrakar@gmail.com>
2026-05-18 17:15:35 +0530

    Studied if else

---
+++ control_workflow/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  display = false;
+  toggleDiv = false;
+
+  toggle() {
+    this.toggleDiv = !this.toggleDiv;
+  }
+}
