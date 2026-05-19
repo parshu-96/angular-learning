23e9e68f7d4ad1309b4af13d42801a88091ff5bc
parshu <parshuvmahindrakar@gmail.com>
2026-05-19 14:28:38 +0530

    For loop

--- control_workflow/src/app/app.component.ts
+++ control_workflow/src/app/app.component.ts
@@ -17,8 +17,19 @@ export class AppComponent {
   // handleInput(event: Event) {
   //   this.color = Number((event.target as HTMLInputElement).value);
   // }
-  color = 'green';
-  handleColor(val: string) {
-    this.color = val;
+  // color = 'green';
+  // handleColor(val: string) {
+  //   this.color = val;
+  // }
+  users = ['Parmanand', 'Prajakta', 'Rohit', 'Mayur'];
+  students = [
+    { name: 'Parmanand', age: 22, email: 'parmanand@example.com' },
+    { name: 'Prajakta', age: 21, email: 'prajakta@example.com' },
+    { name: 'Rohit', age: 23, email: 'rohit@example.com' },
+    { name: 'Mayur', age: 24, email: 'mayur@example.com' },
+  ];
+
+  getName(name: string) {
+    console.log(name);
   }
 }
