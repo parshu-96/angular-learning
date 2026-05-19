d12c2c67cb9480abe33db9182543a20d969c8db7
parshu <parshuvmahindrakar@gmail.com>
2026-05-19 18:00:35 +0530

    Signals introduction

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'signals';
+  count = signal(20);
+  x = 20;
+
+  constructor() {
+    effect(() => {
+      console.log('Signal Value: ', this.count());
+    });
+  }
+
+  incrementCountOfSignal() {
+    this.count.set(this.count() + 1);
+  }
 }
