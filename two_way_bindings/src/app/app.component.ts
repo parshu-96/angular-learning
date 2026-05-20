e82ab4e54714d0cf64e8d959261c3d36c205b6a9
parshu <parshuvmahindrakar@gmail.com>
2026-05-20 17:22:00 +0530

    2 way binding with and without NGModel

--- two_way_bindings/src/app/app.component.ts
+++ two_way_bindings/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'two_way_bindings';
+  name: string = 'Parshu';
+  updateName(event: Event) {
+    this.name = (event.target as HTMLInputElement).value;
+  }
 }
