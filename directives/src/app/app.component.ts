cc396782eb295957a8b70a7356a15030869733ca
parshu <parshuvmahindrakar@gmail.com>
2026-05-21 17:22:42 +0530

    ngIf directive

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -1,12 +1,13 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'directives';
+  show = false;
 }
