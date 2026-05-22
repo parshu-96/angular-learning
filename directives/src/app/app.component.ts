03d86eeed51f58ca4f4271fb2d27f8bf661e9ecf
parshu <parshuvmahindrakar@gmail.com>
2026-05-22 23:00:30 +0530

    ngSwitch

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -1,48 +1,49 @@
-import { NgFor, NgIf, NgClass } from '@angular/common';
+import { NgFor, NgIf, NgClass, NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [NgIf, NgFor, NgClass],
+  imports: [NgIf, NgFor, NgClass, NgSwitch, NgSwitchCase],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   show = false;
-  students = ['Anil', 'Sunil', 'Rohit', 'Suresh', 'Mahesh'];
+  // students = ['Anil', 'Sunil', 'Rohit', 'Suresh', 'Mahesh'];
 
-  studentsData = [
-    {
-      name: 'Anil',
-      age: 25,
-      city: 'Delhi',
-    },
-    {
-      name: 'Sunil',
-      age: 30,
-      city: 'Mumbai',
-    },
-    {
-      name: 'Rohit',
-      age: 28,
-      city: 'Bangalore',
-    },
-    {
-      name: 'Suresh',
-      age: 22,
-      city: 'Chennai',
-    },
-    {
-      name: 'Mahesh',
-      age: 27,
-      city: 'Hyderabad',
-    },
-  ];
+  // studentsData = [
+  //   {
+  //     name: 'Anil',
+  //     age: 25,
+  //     city: 'Delhi',
+  //   },
+  //   {
+  //     name: 'Sunil',
+  //     age: 30,
+  //     city: 'Mumbai',
+  //   },
+  //   {
+  //     name: 'Rohit',
+  //     age: 28,
+  //     city: 'Bangalore',
+  //   },
+  //   {
+  //     name: 'Suresh',
+  //     age: 22,
+  //     city: 'Chennai',
+  //   },
+  //   {
+  //     name: 'Mahesh',
+  //     age: 27,
+  //     city: 'Hyderabad',
+  //   },
+  // ];
 
-  login = true;
-  block = 0;
-  updateBlock() {
-    this.block = this.block + 1;
-  }
+  // login = true;
+  // block = 0;
+  // updateBlock() {
+  //   this.block = this.block + 1;
+  // }
+  color = 'red';
 }
