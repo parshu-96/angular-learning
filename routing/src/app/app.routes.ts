fd4e00be07aef3c5100a0177bcbbab6aca4eb251
parshu <parshuvmahindrakar@gmail.com>
2026-05-22 23:15:47 +0530

    Routing in Angular

--- routing/src/app/app.routes.ts
+++ routing/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'contact',
+    component: LoginComponent,
+  },
+];
