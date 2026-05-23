d33d7a9b208b0a10fc5bf46be4ef6c0acc40a26e
parshu <parshuvmahindrakar@gmail.com>
2026-05-23 08:03:53 +0530

    Create a nav bar and used routeLinkActive and routeLinkActiveOptions

--- routing/src/app/app.routes.ts
+++ routing/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'about',
     component: AboutComponent,
@@ -13,6 +19,6 @@ export const routes: Routes = [
   },
   {
     path: 'contact',
-    component: LoginComponent,
+    component: ContactComponent,
   },
 ];
