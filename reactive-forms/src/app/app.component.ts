8514d985c8fac67914ad19d1a209062d45bd275c
parshu <parshuvmahindrakar@gmail.com>
2026-05-25 21:07:25 +0530

    Reactive Form Grouping

--- reactive-forms/src/app/app.component.ts
+++ reactive-forms/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,10 +9,27 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  name = new FormControl();
-  password = new FormControl();
-  displayValues() {
-    console.log(this.name.value);
-    console.log(this.password.value);
+  // name = new FormControl();
+  // password = new FormControl();
+  // displayValues() {
+  //   console.log(this.name.value);
+  //   console.log(this.password.value);
+  // }
+  profileForm = new FormGroup({
+    name: new FormControl('John Doe'),
+    email: new FormControl('john.doe@example.com'),
+    password: new FormControl('password123'),
+  });
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
+
+  setValues() {
+    this.profileForm.setValue({
+      name: 'Jane Doe 2',
+      email: 'jane.doe@example.com',
+      password: 'newpassword123',
+    });
   }
 }
