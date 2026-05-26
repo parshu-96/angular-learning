ba325c12cc671fcd3d494a2af5cb9342e5bf6038
parshu <parshuvmahindrakar@gmail.com>
2026-05-26 15:54:31 +0530

    Template driven forms

---
+++ template-driven-forms/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  userDetails: any;
+
+  addDetails(val: NgForm) {
+    console.log(val.value);
+    this.userDetails = val.value;
+  }
+}
