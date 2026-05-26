2362ec5c32713654ed3c1f4b7cd706802d37ac7b
parshu <parshuvmahindrakar@gmail.com>
2026-05-26 18:04:52 +0530

    Passing data from parent to child component

---
+++ parent-child-data-exchange/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  userName = 'Bruce';
+  onUserChange(user: string) {
+    this.userName = user;
+  }
+}
