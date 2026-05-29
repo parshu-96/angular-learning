cd2a95174c34bf0f80f015761d911a357e0b2076
parshu <parshuvmahindrakar@gmail.com>
2026-05-29 15:32:20 +0530

    Lifecycle Methods

--- component-lifecycle/src/app/app.component.ts
+++ component-lifecycle/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { afterRender, Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { UserComponent } from './user/user.component';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
+  @ViewChild('user') UserComponent: any;
   title = 'component-lifecycle';
+  counter = 0;
+
+  constructor() {
+    afterRender(() => {
+      console.log('After Re render', this.UserComponent.counter);
+    });
+  }
+
+  incrementCounter() {
+    this.counter++;
+  }
 }
--- component-lifecycle/src/app/user/user.component.ts
+++ component-lifecycle/src/app/user/user.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input() counter = 0;
+  name = 'John Doe';
+  constructor() {
+    console.log('UserComponent constructor called');
+    this.name = 'Sid';
+  }
 
+  ngOnInit() {
+    console.log('UserComponent ngOnInit called');
+    this.name = 'John';
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called');
+  }
+
+  ngOnChanges() {
+    console.log('ngValuesChanges called');
+  }
 }
